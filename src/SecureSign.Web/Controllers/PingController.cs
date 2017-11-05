0ce82bb016a064700ccf40071b1163b64bf1e5b7
Daniel Lo Nigro <d@d.sb>
2017-11-05 14:45:56 -0800

    Add /ping endpoint to check uptime

---
+++ src/SecureSign.Web/Controllers/PingController.cs
@@ -0,0 +1,20 @@
+﻿/**
+ * Copyright (c) 2017 Daniel Lo Nigro (Daniel15)
+ * 
+ * This source code is licensed under the MIT license found in the 
+ * LICENSE file in the root directory of this source tree. 
+ */
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecureSign.Web.Controllers
+{
+	public class PingController : Controller
+	{
+		[Route("ping")]
+		public IActionResult Index()
+		{
+			return Content("Hello from SecureSign");
+		}
+	}
+}
