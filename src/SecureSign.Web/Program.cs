268148e7715b5c7e0ea6d38f57fa4cd6698939ea
Daniel Lo Nigro <d@d.sb>
2017-11-05 15:25:24 -0800

    Save logs to files

--- src/SecureSign.Web/Program.cs
+++ src/SecureSign.Web/Program.cs
@@ -9,51 +9,53 @@ using System;
 using System.Net;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
 using SecureSign.Core.Extensions;
 
 namespace SecureSign.Web
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			BuildWebHost(args).Run();
+		}
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+		public static IWebHost BuildWebHost(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
 				.ConfigureAppConfiguration(builder => builder.AddSecureSignConfig())
-                .UseStartup<Startup>()
+				.ConfigureLogging(builder => builder.AddFile("logs/{Date}.txt"))
+				.UseStartup<Startup>()
 				.UseKestrel(options =>
-	            {
-		            var rawIP = Environment.GetEnvironmentVariable("LISTEN_IP");
-		            if (!string.IsNullOrWhiteSpace(rawIP))
-		            {
-			            var ip = rawIP == "*" ? IPAddress.Any : IPAddress.Parse(rawIP);
+				{
+					var rawIP = Environment.GetEnvironmentVariable("LISTEN_IP");
+					if (!string.IsNullOrWhiteSpace(rawIP))
+					{
+						var ip = rawIP == "*" ? IPAddress.Any : IPAddress.Parse(rawIP);
 						// Check if HTTP port was provided
-			            var httpPort = Environment.GetEnvironmentVariable("HTTP_PORT");
+						var httpPort = Environment.GetEnvironmentVariable("HTTP_PORT");
 						if (!string.IsNullOrWhiteSpace(httpPort))
-			            {
-				            options.Listen(ip, int.Parse(httpPort));
-			            }
+						{
+							options.Listen(ip, int.Parse(httpPort));
+						}
 
 						// Check if HTTPS config was provided
-			            var httpsPort = Environment.GetEnvironmentVariable("HTTPS_PORT");
-			            var certFile = Environment.GetEnvironmentVariable("HTTPS_CERT");
-			            var certPassword = Environment.GetEnvironmentVariable("HTTPS_CERT_PASSWORD");
-			            if (
-				            !string.IsNullOrWhiteSpace(httpsPort) &&
-				            !string.IsNullOrWhiteSpace(certFile) &&
-				            !string.IsNullOrWhiteSpace(certPassword)
-			            )
-			            {
-				            options.Listen(ip, int.Parse(httpsPort), listenOptions =>
-				            {
-					            listenOptions.UseHttps(certFile, certPassword);
-				            });
-			            }
-		            }
-	            })
-                .Build();
-    }
+						var httpsPort = Environment.GetEnvironmentVariable("HTTPS_PORT");
+						var certFile = Environment.GetEnvironmentVariable("HTTPS_CERT");
+						var certPassword = Environment.GetEnvironmentVariable("HTTPS_CERT_PASSWORD");
+						if (
+							!string.IsNullOrWhiteSpace(httpsPort) &&
+							!string.IsNullOrWhiteSpace(certFile) &&
+							!string.IsNullOrWhiteSpace(certPassword)
+						)
+						{
+							options.Listen(ip, int.Parse(httpsPort), listenOptions =>
+							{
+								listenOptions.UseHttps(certFile, certPassword);
+							});
+						}
+					}
+				})
+				.Build();
+	}
 }
