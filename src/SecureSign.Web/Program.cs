d06886c9a185de0b5b0d6990c5770c2258585dba
Daniel Lo Nigro <d@d.sb>
2019-01-26 15:32:55 -0800

    Upgrade to ASP.NET Core 2.2  - Remove Serilog as it seems to be unmaintained  - Use automatic HTTPS on dev

--- src/SecureSign.Web/Program.cs
+++ src/SecureSign.Web/Program.cs
@@ -5,12 +5,8 @@
  * LICENSE file in the root directory of this source tree. 
  */
 
-using System;
-using System.Collections.Generic;
-using System.Net;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Logging;
 using SecureSign.Core.Extensions;
 
 namespace SecureSign.Web
@@ -19,50 +15,12 @@ namespace SecureSign.Web
 	{
 		public static void Main(string[] args)
 		{
-			BuildWebHost(args).Run();
+			CreateWebHostBuilder(args).Build().Run();
 		}
 
-		public static IWebHost BuildWebHost(string[] args) =>
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
 			WebHost.CreateDefaultBuilder(args)
 				.ConfigureAppConfiguration(builder => builder.AddSecureSignConfig())
-				.ConfigureLogging(builder => builder.AddFile(
-					pathFormat: "logs/{Date}.txt",
-					levelOverrides: new Dictionary<string, LogLevel>
-					{
-						{"Microsoft", LogLevel.Warning}
-					}
-				))
-				.UseStartup<Startup>()
-				.UseKestrel(options =>
-				{
-					var rawIP = Environment.GetEnvironmentVariable("LISTEN_IP");
-					if (!string.IsNullOrWhiteSpace(rawIP))
-					{
-						var ip = rawIP == "*" ? IPAddress.Any : IPAddress.Parse(rawIP);
-						// Check if HTTP port was provided
-						var httpPort = Environment.GetEnvironmentVariable("HTTP_PORT");
-						if (!string.IsNullOrWhiteSpace(httpPort))
-						{
-							options.Listen(ip, int.Parse(httpPort));
-						}
-
-						// Check if HTTPS config was provided
-						var httpsPort = Environment.GetEnvironmentVariable("HTTPS_PORT");
-						var certFile = Environment.GetEnvironmentVariable("HTTPS_CERT");
-						var certPassword = Environment.GetEnvironmentVariable("HTTPS_CERT_PASSWORD");
-						if (
-							!string.IsNullOrWhiteSpace(httpsPort) &&
-							!string.IsNullOrWhiteSpace(certFile) &&
-							!string.IsNullOrWhiteSpace(certPassword)
-						)
-						{
-							options.Listen(ip, int.Parse(httpsPort), listenOptions =>
-							{
-								listenOptions.UseHttps(certFile, certPassword);
-							});
-						}
-					}
-				})
-				.Build();
+				.UseStartup<Startup>();
 	}
 }
--- src/SecureSign.Web/Startup.cs
+++ src/SecureSign.Web/Startup.cs
@@ -7,6 +7,7 @@
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SecureSign.Core.Extensions;
@@ -26,7 +27,7 @@ namespace SecureSign.Web
 		public void ConfigureServices(IServiceCollection services)
         {
 			services.AddSecureSignCore(Configuration);
-	        services.AddMvc();
+	        services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 		}
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
